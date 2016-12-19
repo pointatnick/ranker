5ad27e4d72dac2eefa1e4cdf447c46f0e48790ac
pointatnick <nicholashma@gmail.com>
2016-12-19 03:02:23 -0800

    basic inc/dec and input name

--- src/App.js
+++ src/App.js
@@ -3,8 +3,48 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
+      <div className="Room">
+        <Person />
+        <Person />
+        <Person />
+      </div>
+    )
+  }
+}
+
+class Person extends Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0
+    }
+  }
+  // incrementing a person's score
+  onIncrement() {
+    this.setState({score: this.state.score + 1})
+  }
+  // decrementing a person's score
+  onDecrement() {
+    // a person's score can't be negative
+    if (this.state.score > 0) {
+      this.setState({score: this.state.score - 1})
+    }
+  }
+  // input a name
+  setName(e) {
+    this.setState({name: e.target.value})
+  }
+  render() {
+    return (
       <div>
-        Hello world!
+        <div>
+          <input onChange={this.setName.bind(this)} />
+          <br />
+          <button onClick={this.onIncrement.bind(this)}>+</button>
+          <p className="Person-score">{this.state.score}</p>
+          <button onClick={this.onDecrement.bind(this)}>-</button>
+        </div>
+        <div className="Person-name">{this.state.name}</div>
       </div>
     );
   }
