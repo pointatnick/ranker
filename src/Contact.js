0dbf588c0d93a8783843b68c3655fb32658ace66
Daniel <danielwang405@gmail.com>
2016-12-19 16:48:58 -0800

    added routing and filler pages

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Contact extends React.Component {
+	render(){
+		return(
+			<div>Contact Information Here</div>
+			)
+	}
+}
+
+export default Contact
---
+++ src/Ranking.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Ranking extends React.Component {
+	render(){
+		return(
+			<div>Global Rankings found here</div>
+			)
+	}
+}
+
+export default Ranking
